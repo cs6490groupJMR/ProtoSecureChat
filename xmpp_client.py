4ad1ef6146f9df0c2d77afaa3acf018e052bda36
roozbehid <roozbehid@gmail.com>
2014-12-04 18:44:24 -0700

    Update xmpp_client.py
    testing two connections!

--- xmpp_client.py
+++ xmpp_client.py
@@ -101,4 +101,11 @@ if __name__ == '__main__':
     if(len(sys.argv) > 1):
         react(main, sys.argv[1:])
     else:
-        print "Usage: xmpp_client.py xmmpuser@chat.server.com password"
+        username1 = raw_input("Please enter your 1st user name: ")
+		pass1 = raw_input("Please enter your password: ")
+
+		username2 = raw_input("Please enter your 2st user name: ")
+		pass2 = raw_input("Please enter your password: ")
+		
+		react(main,[username1,pass1])
+		react(main,[username2,pass2])
