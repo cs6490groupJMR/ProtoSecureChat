31787631a41905c7ce536933fe0865433dd86e91
Roozbeh <roozbehid@gmail.com>
2014-12-09 16:19:31 -0700

    small bugs fixed

--- xmpp_client.py
+++ xmpp_client.py
@@ -44,7 +44,7 @@ class Client(object):
         self.textColor = cColor
         cColor +=1
         self.reactor = reactor
-        self.proto = protocol.Protocol(s_id)
+        self.proto = protocol.Protocol(self.s_id)
         f = client.XMPPClientFactory(jid, secret)
         f.addBootstrap(xmlstream.STREAM_CONNECTED_EVENT, self.connected)
         f.addBootstrap(xmlstream.STREAM_END_EVENT, self.disconnected)
@@ -80,19 +80,21 @@ class Client(object):
 
     def handle_message(self, message):
         screen.set_color(self.textColor, 0)
+        print 'a message is recieved :'
+        print message
 
         for element in message.elements():
           if element.name == 'body':
             body = unicode(element).strip()
             answer = self.proto.processIncomingMSG_and_Answer(body)
-            print 'a message is recieved :'
-            print body
+            
+            
             if (answer[0]!=""):
                 print answer[0]
 
             for txt in answer[1:]:
                 if (txt!=""):
-                    self.sendMessage(self.chatbuddy, txt)
+                    self.sendMessage(self.chatbuddy_jid, txt)
 
             #self.send_message(message['from'], 'tetet')
             break
@@ -109,7 +111,7 @@ class Client(object):
             return
         screen.set_color(self.textColor, 0)
         message = domish.Element((None, 'message'))
-        message['to'] = to
+        message['to'] = to.full()
         #google doesnt like from tag, it should acompany id too! just ignoring it seems to work!
         #message['from'] = self.jid.full()
         message['type'] = 'chat'
