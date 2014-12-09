cf1fa9bd00e820b598bf05963753f0d235f7d8ad
mcarter <montgomery.carter@gmail.com>
2014-12-08 19:24:51 -0700

    Updated paper
    Protocol now has name.
    Added a bunch of stuff to paper, reorganized, etc.

--- Report/auto/ProjectReport.el
+++ Report/auto/ProjectReport.el
@@ -1,12 +1,15 @@
 (TeX-add-style-hook "ProjectReport"
  (lambda ()
     (LaTeX-add-bibliographies)
+    (LaTeX-add-environments
+     "Figure")
     (LaTeX-add-labels
+     "sec:protoDef"
      "wbp:beginDH"
-     "wbp:beginNonceExchange")
-    (TeX-add-symbols
-     "ScaleIfNeeded"
-     "origincludegraphics")
+     "wbp:beginNonceExchange"
+     "fig:fullProto"
+     "fig:basicProto"
+     "fig:sslProto")
     (TeX-run-style-hooks
      "geometry"
      "columnsep=20pt"
@@ -18,11 +21,13 @@
      "parskip"
      "fouriernc"
      "amsmath"
+     "caption"
      "graphicx"
      "multicol"
      ""
      "latex2e"
      "art10"
      "article"
+     "10pt"
      "twoside")))
 
